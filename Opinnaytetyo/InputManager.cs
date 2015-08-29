c3bbc7e32e554226744e0e9c2a1cde5b68470371
Olli <opinnaytepaahto@gmail.com>
2015-08-29 21:11:07 +0300

    Lisätty isKeyJustDown funktio

--- Opinnaytetyo/InputManager.cs
+++ Opinnaytetyo/InputManager.cs
@@ -9,15 +9,27 @@ namespace Opinnaytetyo
     class InputManager
     {
         private static KeyboardState kbstate;
+        private static KeyboardState prevKbstate;
 
         public static void update()
         {
+            prevKbstate = kbstate;
             kbstate = Keyboard.GetState();
         }
 
         public static bool isKeyDown(Keys key)
         {
             return kbstate.IsKeyDown(key);
+        }
+
+        public static bool isKeyJustDown(Keys key)
+        {
+            if (kbstate.IsKeyDown(key) && !prevKbstate.IsKeyDown(key))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public static bool isKeyUp(Keys key)
